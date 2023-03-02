85f88fbdd0f015d522b2a352addcf9cfaddf11b1
AliSenina <122289718+AliSenina@users.noreply.github.com>
2023-03-02 18:42:55 +0200

    Add files via upload

---
+++ scriptHW.js
@@ -0,0 +1,49 @@
+// Task 1
+
+// let str = prompt("Введіть текст")
+
+// let count = 0
+// let position = str.indexOf(' ')
+
+// while (position !== -1) {
+//     count++
+//     position = str.indexOf(' ', position + 1)
+// }
+// console.log(count)
+
+
+// Task 2
+// let result = prompt(`Enter your email`);
+// let doc = result.indexOf('@')
+
+// if (doc == -1 || doc == 0 || doc ==(result.length - 1)) {
+//     console.log('Емейл хибний');
+
+// }
+
+// else {
+//     console.log('Емейл прийнято')
+// }
+
+// Task 3
+// let str = prompt("Введіть текст")
+
+// let count = 0
+// let position = str.indexOf('html')
+
+// while (position !== -1) {
+//     count++
+//     position = str.indexOf('html', position + 1)
+// }
+// console.log(count)
+
+// Task 4
+// let str = prompt("Введіть посилання")
+// if (str.includes('https://')) {
+//     console.log(str.slice(8))
+// }
+// if(str.includes('http://')) {
+//     console.log(str.slice(7))
+// }
+
+
